52c93e6072fa37eab64959cbee6843d19c281abc
0x4c5afb <cool_code@sina.com>
2023-05-12 21:06:32 +0800

    feat: 如何触发更新 探索mount流程

---
+++ packages/react-reconciler/src/beginWork.ts
@@ -0,0 +1,55 @@
+import { UpdateQueue, processUpdateQueue } from './updateQueue';
+import { FiberNode } from './fiber';
+import { HostComponent, HostRoot, HostText } from './workTags';
+import { ReactElementType } from 'shared/ReactTypes';
+import { mountChildFibers, reconcileChildFibers } from './childFibers';
+
+// 递归中的递阶段
+export const beginWork = (wip: FiberNode) => {
+	// 比较 然后返回子FiberNode
+	switch (wip.tag) {
+		case HostRoot:
+			return updateHostRoot(wip);
+		case HostComponent:
+			return updateHostComponent(wip);
+		case HostText:
+			return null;
+		default:
+			if (__DEV__) {
+				console.log('beginWork未实现的类型');
+			}
+			break;
+	}
+	return null;
+};
+
+function updateHostRoot(wip: FiberNode) {
+	const baseState = wip.memoizedProps;
+	const updateQueue = wip.updateQueue as UpdateQueue<Element>;
+	const pending = updateQueue.shared.pending;
+	updateQueue.shared.pending = null;
+	const { memoizedState } = processUpdateQueue(baseState, pending);
+	wip.memoizedState = memoizedState;
+
+	const nextChildren = wip.memoizedState;
+	reconcileChildren(wip, nextChildren);
+	return wip.child;
+}
+
+function updateHostComponent(wip: FiberNode) {
+	const nextProps = wip.pendingProps;
+	const nextChildren = nextProps.children;
+	reconcileChildren(wip, nextChildren);
+	return wip.child;
+}
+
+function reconcileChildren(wip: FiberNode, children?: ReactElementType) {
+	const current = wip.alternate;
+	if (current !== null) {
+		//   update
+		wip.child = reconcileChildFibers(wip, current?.child, children);
+	} else {
+		// mount
+		wip.child = mountChildFibers(wip, null, children);
+	}
+}
