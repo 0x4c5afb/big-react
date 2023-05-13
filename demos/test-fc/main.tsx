b6ea4ac851348295371614ab59aefb78bf6caa9c
0x4c5afb <cool_code@sina.com>
2023-05-13 23:13:21 +0800

    feat: 实现useState

--- demos/test-fc/main.tsx
+++ demos/test-fc/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 
 function App() {
+  const [num, setNum] = useState(100);
+  window.setNum = setNum;
   return (
     <div>
-      <span>big-react</span>
+      {/* <span>big-react</span> */}
+      {num}
     </div>
   );
 }
